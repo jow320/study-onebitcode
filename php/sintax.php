536a0c8df6746fca4cfe8c3244d64556a44bb315
Victor Estevan <jow_d3@hotmail.com>
2026-09-03 18:19:56 -0300

    php #1

---
+++ php/sintax.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <?php 
+    //Comentários
+    $var1 = 10 + 5;
+    echo "Olá mundo"; 
+
+    echo "O resultado da variável é: " . $var1;
+    ?>
+    
+  </body>
+</html>
